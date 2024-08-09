7edfefc202115a557250062a8ba11d4a11016963
iTrong <trongtrong03@gmail.com>
2024-08-09 18:23:27 +0800

    20240809-v1

---
+++ src/views/hikingbook/mtlogs/articles/009.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// 引用組件
+import BaseTextContent from '@/components/Common/BaseTextContent.vue';
+import Title from "@/components/Common/BaseTextTitle.vue";
+</script>
+
+<template>
+    <BaseTextContent>
+        <Title :propValue="9" fileType="mountainsLogs" />
+<!-- start -->
+<div class="text-content">
+    <div class="text-block">
+        <h3>序、楔子</h3>
+        <p>當與攀岩愛好者開啟台灣有哪些著名天然岩場的話匣子時，北部人可能會回答你皇帝殿、五寮尖、孝子山，或是近年很夯的劍龍稜；南部人則可能給出關子嶺岩場、獵鷹尖等答案。那麼中部呢？「鳶嘴山」這個名字肯定當仁不讓。且不侷限於攀岩界，假如上登山社群發問中級山的推薦名單，鳶嘴山肯定也是榜上有名──不光要面對各種岩石地形的阻礙才能攀上顛峰，下山則還得必須具備足夠的膽識。早在多年前，鳶嘴山已是許多喜愛冒險的登山人倍加推崇的中級山，每逢例假日，前來攀爬鳶嘴山的挑戰者可說是絡繹不絕，總是將狹窄的石道擠得水洩不通，足見這座譽為中部最美中級山之魅力。</p>
+        <p>除了鳶嘴山，其所途經的大雪山林道上還有許多適合闔家大小攀登健行的山岳，緊鄰鳶嘴山隔壁的稍來山便是其一。稍來山不僅是小百岳一員，同時也是名聞遐邇的賞楓勝地，因此大多數登山客會將鳶嘴與稍來二山安排在一起縱走，而這條路線如今已規劃為「鳶嘴稍來山國家步道」，儼然是中部地區極富盛名的中海拔熱門登山路線。</p>
+    </div>
+    <div class="text-block">
+        <h3>一、補班日</h3>
+        <p>在我的登山生涯裡，鳶嘴山算是比較早期挑戰的山岳，但回憶起剛踏入登山界的時候，礙於怕高，其實沒想過也不敢想自己會有挑戰祂的一天，</p>
+        <figure>
+            <img src="/images/mtlogs/009/01.jpg">
+        </figure>
+    </div>
+</div>
+<!-- end -->
+    </BaseTextContent>
+</template>
