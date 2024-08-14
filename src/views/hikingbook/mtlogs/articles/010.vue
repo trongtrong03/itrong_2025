1c80843c1ca7efa427a16b3000fec256db82837d
iTrong <trongtrong03@gmail.com>
2024-08-14 08:36:22 +0800

    20240814-v1

---
+++ src/views/hikingbook/mtlogs/articles/010.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// 引用組件
+import BaseTextContent from '@/components/Common/BaseTextContent.vue';
+import Title from "@/components/Common/BaseTextTitle.vue";
+</script>
+
+<template>
+    <BaseTextContent>
+        <Title :propValue="10" fileType="mountainsLogs" />
+<!-- start -->
+<div class="text-content">
+    <div class="text-block">
+        <h3>序、楔子</h3>
+        <p>當與攀岩愛好者開啟台灣有哪些著名天然岩場的話匣子時，北部人可能會回答你皇帝殿、五寮尖、孝子山，或是近年很夯的劍龍稜；南部人則可能給出關子嶺岩場、獵鷹尖等答案。那麼中部呢？「鳶嘴山」這個名字肯定當仁不讓。且不侷限於攀岩界，假如上登山社群發問中級山的推薦名單，鳶嘴山肯定也是榜上有名──不光要面對各種岩石地形的阻礙才能攀上顛峰，下山則還得必須具備足夠的膽識。早在多年前，鳶嘴山已是許多喜愛冒險的登山人倍加推崇的中級山，每逢例假日，前來攀爬鳶嘴山的挑戰者可說是絡繹不絕，總是將狹窄的石道擠得水洩不通，足見這座譽為中部最美中級山之魅力。</p>
+    </div>
+    <div class="text-block">
+        <h3>一、補班日</h3>
+        <figure>
+            <img src="/images/mtlogs/009/10.jpg">
+        </figure>
+    </div>
+</div>
+<!-- end -->
+    </BaseTextContent>
+</template>
